5c32a9fcee5281c8ff9d3ff7d99d5be906128bfd
João Paulo Nobrega <joao.paulo.nobrega@avanade.com>
2018-07-13 15:54:39 -0300

    secretkey

--- src/api/controllers/login-controller.js
+++ src/api/controllers/login-controller.js
@@ -12,3 +12,14 @@ const loginController = (req, res) => {
 module.exports = {
     loginController: loginController
 };
+
+// Exemplo puxar arquivo do bucket
+/*
+const s3 = new AwsBucket();
+    s3.load(`gsp/products/${ searchText }.json`, (err, data) => {
+        if (err)
+            return res.status(204).send({ message: err.message });
+
+        return res.status(200).send({ data });
+    }); 
+*/
